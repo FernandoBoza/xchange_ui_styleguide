6880487b490794433d3368095a8450f67cc6ee4e
Primestream Development <development@Primestreams-Mac-mini.local>
2019-04-15 16:51:44 -0400

    changed icons to svg

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-$('.trigger').click(function () {
-   $('ul.dropdown_list').toggleClass("show");
-   $(this).parent().toggleClass("show");
-   $('.fal').toggleClass('fa-chevron-circle-up')
+$(".trigger").click(function() {
+  $("ul.dropdown_list").toggleClass("show");
+  $(this)
+    .parent()
+    .toggleClass("show");
+  $(".fal").toggleClass("fa-chevron-circle-up");
 });
